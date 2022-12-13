aa7260254efe13eb850043c6f86b4a40b753e636
martinmmi <martin.mittrenga@t-online.de>
2022-12-13 04:32:24 +0100

    Discover for 1 device implemented

--- src/main.cpp
+++ src/main.cpp
@@ -19,42 +19,23 @@ const int csPin = 18;          // LoRa radio chip select
 const int resetPin = 23;       // LoRa radio reset
 const int irqPin = 26;         // change for your board; must be a hardware interrupt pin
 
-int rssi;
-int snr;
-int rn;
-int first_data_int;
-int secound_data_int;
+String outgoing;              // outgoing message
+String mode;
 
-int random_number = random(0, 100);
-
-char delim[] = "/";
-char buf1[16];
-char buf2[8];
-char buf3[8];
-char buf_rn[8];
-char data[16];
-
-char typ;
-char first_adress;
-char first_data;
-char secound_adress;
-char secound_data;
+char buf_sm[12];
+char buf_rm[12];
 
 bool clk_state;
 bool last_clk_state;
-bool high = 1;
-bool low = 0;
 
-int defaultBrightness = 50;
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xAA;      // destination to send to
+long lastDiscoverTime = 0;    // last send time
+long lastClockTime = 0;
+int interval = 2000;          // interval between sends
+int counterOffer = 0;
 
-unsigned long lastMillisLoop = 0;
-unsigned long lastMillisLed = 0;
-unsigned long lastMillisTakt_1 = 0;
-unsigned long lastMillisTakt_2 = 0;
-
-//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* reset=*/ 8);
-//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
-//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 16, /* data=*/ 17, /* reset=*/ U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);   // ESP32 Thing, HW I2C with pin remapping
 
 #define RELAI_PIN           14
@@ -67,29 +48,83 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 uint32_t red = strip.Color(255, 0, 0);
 uint32_t green = strip.Color(0, 255, 0);
 uint32_t blue = strip.Color(0, 0, 255);
-uint32_t yellow = strip.Color(255, 170, 0);
+uint32_t yellow = strip.Color(255, 50, 0);
 uint32_t nocolor = strip.Color(0, 0, 0);
 
+int defaultBrightness = 100;
 
 //////////////////////////////////////////////////////////////////////
 
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
 
-void printDisplay() {
+//////////////////////////////////////////////////////////////////////
 
-    u8g2.clearBuffer();					// clear the internal memory
-    u8g2.clear();
-    u8g2.setFont(u8g2_font_6x13_tf);	// choose a suitable font
-    u8g2.drawStr(0,10,"LoRa Receiver");	// write something to the internal memory
-    u8g2.drawStr(0,30,buf1);
-    u8g2.drawStr(0,50,"RSSI:");
-    u8g2.drawStr(35,50,buf2);
-    u8g2.drawStr(70,50,"SNR:");
-    u8g2.drawStr(105,50,buf3);
-    u8g2.drawStr(105,30,buf_rn);
-    u8g2.sendBuffer();					// transfer internal memory to the display
+String onReceive(int packetSize) {
+  if (packetSize == 0) return "";          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return "";                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return "";                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+
+  return incoming;
 
 }
 
+//////////////////////////////////////////////////////////////////////
+
+void printDisplay(String rm, String sm) {
+
+  sprintf(buf_rm, "%s", rm);
+  sprintf(buf_sm, "%s", sm);
+
+  u8g2.clearBuffer();					                    // clear the internal memory
+  u8g2.setFont(u8g2_font_6x13_tf);
+  u8g2.drawStr(0,10,"tallyWAN_receiver");	    // write something to the internal memory
+  u8g2.drawStr(0,25,"RxD:");
+  u8g2.drawStr(35,25,buf_rm);
+  u8g2.drawStr(0,40,"TxD:");
+  u8g2.drawStr(35,40,buf_sm);
+  u8g2.sendBuffer();
+
+}
 
 //////////////////////////////////////////////////////////////////////
 
@@ -99,158 +134,158 @@ void tally(uint32_t color) {
   strip.show();
 }
 
+void tallyBlinkSlow(uint32_t color) {
+  //CLOCK
+  if (clk_state == 0 && millis() - lastClockTime < 1000) {clk_state = 1;
+  }
+    
+  if (clk_state == 1 && millis() - lastClockTime >= 1000) {
+    if(millis() - lastClockTime <= 2000) {clk_state = 0;}
+  }
+    
+  if (last_clk_state != clk_state) {
+    last_clk_state = clk_state;
+    if (clk_state == 1) {strip.fill(color, 0, LED_COUNT);}
+    if (clk_state == 0) {strip.fill(nocolor, 0, LED_COUNT);}
+  }
+
+  if (millis() - lastClockTime >= 2000){lastClockTime = millis();}
+  strip.show();
+}
+
+void tallyBlinkFast(uint32_t color) {
+  //CLOCK
+  if (clk_state == 0 && millis() - lastClockTime < 250) {clk_state = 1;
+  }
+    
+  if (clk_state == 1 && millis() - lastClockTime >= 250) {
+    if(millis() - lastClockTime <= 500) {clk_state = 0;}
+  }
+    
+  if (last_clk_state != clk_state) {
+    last_clk_state = clk_state;
+    if (clk_state == 1) {strip.fill(color, 0, LED_COUNT);}
+    if (clk_state == 0) {strip.fill(nocolor, 0, LED_COUNT);}
+  }
+
+  if (millis() - lastClockTime >= 500){lastClockTime = millis();}
+  strip.show();
+}
 
 void relai(bool state) {
   digitalWrite(RELAI_PIN, state);
 }
 
-
 //////////////////////////////////////////////////////////////////////
-
-
-void onReceive(int packetSize) {
-  if (packetSize == 0) return;          // if there's no packet, return
-
-  String incoming = "";
-
-  while (LoRa.available()) {
-    incoming += (char)LoRa.read();
-  }
-
-  Serial.print("Received Message: '" + incoming);
-  Serial.print("' with RSSI: " + String(LoRa.packetRssi()));
-  Serial.print(" and SNR: " + String(LoRa.packetSnr()));
-  Serial.println();
-
-  rssi = (LoRa.packetRssi());
-  snr = (LoRa.packetSnr());
-  rn = random_number;
-
-  sprintf(buf1, "%s", incoming);
-  sprintf(buf2, "%d", rssi);
-  sprintf(buf3, "%d", snr);
-  sprintf(buf_rn, "%d", rn);
-  sprintf(data, "%s", incoming);
-
-  printDisplay();
-  digitalWrite(LED_PIN_INTERNAL, HIGH);
-
-  char *typ = strtok(data, delim);
-  char *first_adress = strtok(NULL, delim);
-  char *first_data = strtok(NULL, delim);
-  char *secound_adress = strtok(NULL, delim);
-  char *secound_data = strtok(NULL, delim);
-
-  sscanf(first_data, "%d", &first_data_int);
-  sscanf(secound_data, "%d", &secound_data_int);
-
-  //if (test.equals(first_data))
-  if (secound_data_int == random_number) {
-    tally(red);
-    relai(high);
-  }
-  else {
-    tally(nocolor);
-    relai(low);
-  }
-
-}
-
-
-//////////////////////////////////////////////////////////////////////
-
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);                   // initialize serial
   while (!Serial);
 
-  lastMillisLoop = millis();
-  lastMillisLed = millis();
-  lastMillisTakt_1 = millis();
-  lastMillisTakt_2 = millis();
+  mode = "nothing";
 
-  Serial.println("LoRa Receiver");
+  Serial.println("");
+  Serial.println("tallyWAN_receiver");
 
+  // override the default CS, reset, and IRQ pins (optional)
   LoRa.setPins(csPin, resetPin, irqPin); // set CS, reset, IRQ pin
-
   LoRa.setTxPower(17);  //2-20 default 17
   LoRa.setSpreadingFactor(7);    //6-12 default 7
   LoRa.setSignalBandwidth(125E3);   //7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, and 500E3 default 125E3
   LoRa.setCodingRate4(5);   //5-8 default 5
   LoRa.setPreambleLength(8);    //6-65535 default 8
-
   LoRa.begin(868E6);  //set Frequenz 915E6 or 868E6
 
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed");
-    while (1);
+  if (!LoRa.begin(868E6)) {             // initialize ratio at 868 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
   }
 
-  u8g2.begin();     //display begin
+  Serial.println("LoRa init succeeded.");
 
-  strip.begin();    //led begin
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tf);	
+  u8g2.drawStr(0,10,"tallyWAN_receiver");
+  u8g2.drawStr(0,25,"TxD:");
+  u8g2.drawStr(0,40,"RxD:");
+  u8g2.sendBuffer();
+
+  Serial.println("OLED init succeeded.");
+
+  strip.begin();    
   strip.setBrightness(defaultBrightness);    
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show();
 
-  pinMode(RELAI_PIN, OUTPUT);
+  Serial.println("Strip init succeeded.");
+
   pinMode(LED_PIN_INTERNAL, OUTPUT);
+  pinMode(RELAI_PIN, OUTPUT);
 
 }
-
 
 //////////////////////////////////////////////////////////////////////
 
-
 void loop() {
-  
-  onReceive(LoRa.parsePacket());
 
-  //MILLIS CLOCK
-  if ((millis() - lastMillisLoop) >= 10000) {
-    random_number = random(0, 100);
-    Serial.print("CHANGE:"); Serial.println(random_number);
-    lastMillisLoop = millis();
+  //Nothing Mode
+  if ((mode == "nothing")) {
+    String incoming = onReceive(LoRa.parsePacket());
+    printDisplay(incoming, "");
+    tallyBlinkSlow(yellow);
+
+    if (incoming == "dis-anyrec?") {
+      mode = "offer";
+    }
+
   }
 
+  //Discover Mode
+  if ((mode == "discover")) {
+    // parse for a packet, and call onReceive with the result:
+    String incoming = onReceive(LoRa.parsePacket());
+    printDisplay(incoming, "");
+    tallyBlinkFast(yellow);
 
-  if ((millis() - lastMillisLed) >= 500) {
+    if (incoming == "dis-anyrec?") {
+      mode = "offer";
+    }
+
+  }
+
+  //Offer Mode
+  if (mode == "offer") {
+    digitalWrite(LED_PIN_INTERNAL, HIGH);
+    String message = "off-tally1";   // send a message
+    sendMessage(message);
+    printDisplay("", message);
+    Serial.println("TxD: " + message);
     digitalWrite(LED_PIN_INTERNAL, LOW);
-    lastMillisLed = millis();
+
+    counterOffer++;
+    Serial.println(counterOffer);
+    mode = "discover";
+
+    if (counterOffer >= 3) {
+      digitalWrite(LED_PIN_INTERNAL, HIGH);
+      counterOffer = 0;
+      mode = "request";
+    }
+
   }
 
-  //CLOCK
-  if (clk_state == 0 && millis() - lastMillisTakt_1 < 1000) {
-    clk_state = 1;
-  }
+  //Request Mode
+  if ((mode == "request")) {
+    tally(yellow);
     
-  if (clk_state == 1 && millis() - lastMillisTakt_1 >= 1000) {
-    if(millis() - lastMillisTakt_1 <= 2000) {
-    clk_state = 0;
-    }
-  }
-    
-  if (last_clk_state != clk_state) {   
-    last_clk_state = clk_state;
-    if (clk_state == 1) {
-      //tally(red);
-      //relai(clk_state);
-
-    }
-    if (clk_state == 0) {
-      //tally(nocolor);
-      //relai(clk_state);
-
-    }
   }
 
-  if (millis() - lastMillisTakt_1 >= 2000){
-    lastMillisTakt_1 = millis();
+  //Acknowledge Mode
+  if ((mode == "acknowledge")) {
+  
   }
-
 
 }
-
 
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
